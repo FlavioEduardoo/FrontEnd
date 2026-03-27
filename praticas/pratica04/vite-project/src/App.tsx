c356af0f530e9752f5542f6a616e5e510290d99b
Flávio Eduardo <flavioeduardo0811@gmail.com>
2026-03-26 21:50:10 -0300

    terminar em casa

--- praticas/pratica04/vite-project/src/App.tsx
+++ praticas/pratica04/vite-project/src/App.tsx
@@ -1,9 +1,20 @@
-import { Heading } from './components/Heading';
+//import { Heading } from './components/Heading';
+//import { Timer } from 'lucide-react';
+import { Container } from './Container';
+import { Heading } from './Heading';
 
 export function App() {
   return (
     <>
-      <Heading />
+      {/* Seção 1: Logo */}
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      {/* Seção 2: Menu */}
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
